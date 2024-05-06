5527dfb226d2798bb3e36688d25ba660ed00368f
19turgut <turqutbagirli917@gmail.com>
2024-05-06 16:09:10 +0400

    second message

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Modal from "./modal";
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className="app">
+      
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={openModal}
+          >
+            Уведомления
+          </button>
+        </div>
+      </div>
+
+      {showModal && (
+        <Modal onClose={closeModal} />
+      )}
+
+      {showModal && (
+        <div className="modal-backdrop" onClick={closeModal} />
+      )}
+      
+    </div>
+  );
+  <Modal/>
+}
+
+export default App;
